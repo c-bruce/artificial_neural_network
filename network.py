58cb322963965740636ad68af8b2a0367f086896
c-bruce <callum.bruce1@gmail.com>
2023-07-28 17:14:41 +0100

    Started implementing training methods

--- network.py
+++ network.py
@@ -8,6 +8,7 @@ class Network:
         self.biases = self.__init_biases()
         self.dcost_dweights = self.__init_dcost_dweights()
         self.dcost_dbiases = self.__init_dcost_dbiases()
+        self.costs = []
     
     def __init_activations(self):
         activations = []
@@ -45,21 +46,21 @@ class Network:
     def dsigmoid(self, x):
         return np.exp(x) / (np.exp(x) + 1)
     
+    def calculate_cost(self, expected_output):
+        return sum((self.activations[-1] - expected_output)**2)
+    
     def feedforward(self, input_layer):
         self.activations[0] = input_layer
         for i in range(0, len(self.layers) - 1):
             self.activations[i+1] = self.sigmoid(np.dot(self.weights[i], self.activations[i]) + self.biases[i])
     
-    def cost(self, expected):
-        return sum((self.activations[-1] - expected)**2)
-    
-    def backpropagation(self, expected):
+    def backpropagation(self, expected_output):
         # 1) Calculate dcost_dweights and dcost_dbiases for each training example in a batch
         # 2) Add them together and average them
         # 3) Output the gradients for the gradient descent algorithm to nudge them
 
         # Calculate dcost_dactivations for the output layer
-        dcost_dactivations = 2 * (self.activations[-1] - expected)
+        dcost_dactivations = 2 * (self.activations[-1] - expected_output)
 
         # Loop backward through the layers to calculate dcost_dweights and dcost_dbiases
         for i in range(-1, -len(self.layers), -1):
@@ -73,3 +74,39 @@ class Network:
             # Calculate dcost_dactivations for hidden layer
             dz_dactivations = self.weights[i]
             dcost_dactivations = sum(dz_dactivations * (dactivations_dz * dcost_dactivations)[:,np.newaxis])
+    
+    def average_gradients(self, n):
+        # Calculate the average gradients for a batch containing n samples
+        for i in range(0, len(self.layers) - 1):
+            self.dcost_dweights[i] = self.dcost_dweights[i] / n
+            self.dcost_dbiases[i] = self.dcost_dbiases[i] / n
+    
+    def reset_gradients(self):
+        # Reset gradients before starting a new batch
+        self.dcost_dweights = self.__init_dcost_dweights
+        self.dcost_dweights = self.__init_dcost_dweights
+    
+    def update_weights_biases(self):
+        # Perform gradient descent step
+        # Need to implement this method
+        d = 0
+    
+    def process_batch(self, batch):
+        for sample in batch:
+            self.feedforward(sample.input_layer)
+            self.backpropagation(sample.expected_output)
+            batch.cost += self.calculate_cost(sample.expected_output)
+    
+    def train_network(self, epochs, batches):
+        for i in range(0, epochs):
+            print(f"Epoch: {i}\n")
+            for batch in batches:
+                self.process_batch(batch)
+                self.costs.append(batch.cost / len(batch))
+                batch.reset_cost()
+                self.average_gradients(len(batch))
+                self.update_weights_biases()
+                self.reset_gradients()
+
+# Need to implement sample class
+# Need to implement batch class
